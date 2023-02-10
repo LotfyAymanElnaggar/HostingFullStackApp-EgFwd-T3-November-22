c61bf50f5fae8b7507cac7de3427f729b5d6388c
Lotfy Ayman <Lotfyaymanelnaggar@gmail.com>
2023-02-10 17:24:49 +0200

    Update sequelize.ts

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: Number(config.db_port),
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${Number(config.db_port)}/${config.database}`);
+
+// {
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port: Number(config.db_port),
+//   dialect: "postgres",
+//   storage: ":memory:",
+// }
