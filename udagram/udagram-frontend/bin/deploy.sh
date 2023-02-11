495e965896eb91c60decb310474620f99a2692ec
LotfyAyman <lotfyaymanelnaggar@gmail.com>
2023-02-11 21:29:14 +0200

    Remove /index from the bottom line.

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read s3://egfwd-t3-november-22-udagram/
+aws s3 cp --recursive --acl s3://egfwd-t3-november-22-udagram/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www s3://egfwd-t3-november-22-udagram/
